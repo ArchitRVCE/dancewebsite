43e25815ce1e132297713c417e51d8949d3f708f
Archit <architchaturvedi2000@gmail.com>
2023-07-01 22:03:46 +0530

    pushing all files untill 1 July

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ app.set('views',path.join(__dirname,'views'));
 //Endoint
 app.get('/',(req,res)=>{
     const params = {};
-    res.status(200).render('index.pug',params);
+    res.status(200).render('home.pug',params);
+});
+
+app.get('/contact',(req,res)=>{
+    const params = {};
+    res.status(200).render('contact.pug',params);
 });
 app.listen(port,()=>{
     console.log(`The application started uccesfully on : ${port}`);
